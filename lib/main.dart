f9543e7f245af3b57ef603cd9c8a94deafb31f3a
Indra Oki Sandy <indraokisandy1@gmail.com>
2022-09-20 21:15:27 +0700

    Tugas Latihan 5

---
+++ lib/main.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.yellow,
+      ),
+      home: const MyHomePage(title: 'Halo saya latihan'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Center(
+          child: Text(widget.title,
+              style: const TextStyle(fontWeight: FontWeight.bold)),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            const Text('Saya widget ditengah'),
+            Container(
+              height: 30,
+              decoration: BoxDecoration(
+                  color: Colors.redAccent,
+                  border: Border.all(color: Colors.redAccent)),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                const Text('saya kiri'),
+                const Text('saya kanan'),
+              ],
+            ),
+            Column(
+              children: [
+                Container(
+                  height: 40,
+                  padding: const EdgeInsets.fromLTRB(5, 5, 5, 5),
+                  decoration: BoxDecoration(
+                      color: Colors.yellowAccent,
+                      border: Border.all(
+                          color: const Color.fromRGBO(255, 255, 0, 1))),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.purpleAccent,
+                        border: Border.all(
+                          color: Colors.purple,
+                        )),
+                    child: const Center(child: Text('Saya berwarna')),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height / 1.8,
+            ),
+            SizedBox(height: 17,),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Container(
+                    height: MediaQuery.of(context).size.height / 8,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                        color: Colors.black,
+                        border: Border.all(color: Colors.black)),
+                    child: const Center(
+                      child: Text(
+                        'Saya dibawah sendiri',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Increment',
+        child: const Icon(Icons.abc),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
